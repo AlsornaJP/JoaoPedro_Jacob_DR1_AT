4c519288c33aba973ada43e40e0f72ac761a88b0
AlsornaJP <jpribeirojacob@gmail.com>
2026-03-28 19:40:49 -0300

    Exerício 7 concluído

---
+++ src/Exercicios7_12/Funcionario.java
@@ -0,0 +1,47 @@
+package Exercicios7_12;
+
+public abstract class Funcionario {
+    //Construtor
+    //----------------
+    public Funcionario(String nome, String cpf, String sobrenome, double salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.sobrenome = sobrenome;
+        this.salario = salario;
+    }
+    //Atributos
+    //----------------
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private double salario;
+    //Getters
+    //----------------
+    public String getNomeCompleto() {
+        return nome + " " + sobrenome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    //Setters
+    //----------------
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    //Métodos
+    //----------------
+    public abstract double calcularSalario();
+
+}
